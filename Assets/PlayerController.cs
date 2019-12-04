2eb3c3542b1f16fb8c4aeb45e1e63fee927d72c7
YdeaGames <ydeagames@gmail.com>
2019-12-04 20:48:58 +0900

    Prototype

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int id;
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis($"Joy{id}X");
+        var y = Input.GetAxis($"Joy{id}Y");
+        //Debug.Log($"x:{x.ToString("F2")}, y:{y.ToString("F2")}");
+        rigid.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/TrackTargets.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class TrackTargets : MonoBehaviour
+{
+    public Transform[] targets;
+
+    public float boundingBoxPadding = 2f;
+
+    public float minimumOrthographicSize = 8f;
+
+    public float zoomSpeed = 20f;
+
+    Camera cameraObj;
+
+    void Awake()
+    {
+        cameraObj = GetComponent<Camera>();
+        cameraObj.orthographic = true;
+    }
+
+    void LateUpdate()
+    {
+        Rect boundingBox = CalculateTargetsBoundingBox();
+        transform.position = CalculateCameraPosition(boundingBox);
+        cameraObj.orthographicSize = CalculateOrthographicSize(boundingBox);
+    }
+
+    /// <summary>
+    /// Calculates a bounding box that contains all the targets.
+    /// </summary>
+    /// <returns>A Rect containing all the targets.</returns>
+    Rect CalculateTargetsBoundingBox()
+    {
+        float minX = Mathf.Infinity;
+        float maxX = Mathf.NegativeInfinity;
+        float minY = Mathf.Infinity;
+        float maxY = Mathf.NegativeInfinity;
+
+        foreach (Transform target in targets)
+        {
+            Vector3 position = target.position;
+
+            minX = Mathf.Min(minX, position.x);
+            minY = Mathf.Min(minY, position.y);
+            maxX = Mathf.Max(maxX, position.x);
+            maxY = Mathf.Max(maxY, position.y);
+        }
+
+        return Rect.MinMaxRect(minX - boundingBoxPadding, maxY + boundingBoxPadding, maxX + boundingBoxPadding, minY - boundingBoxPadding);
+    }
+
+    /// <summary>
+    /// Calculates a camera position given the a bounding box containing all the targets.
+    /// </summary>
+    /// <param name="boundingBox">A Rect bounding box containg all targets.</param>
+    /// <returns>A Vector3 in the center of the bounding box.</returns>
+    Vector3 CalculateCameraPosition(Rect boundingBox)
+    {
+        Vector2 boundingBoxCenter = boundingBox.center;
+
+        return new Vector3(boundingBoxCenter.x, boundingBoxCenter.y, -10f);
+    }
+
+    /// <summary>
+    /// Calculates a new orthographic size for the camera based on the target bounding box.
+    /// </summary>
+    /// <param name="boundingBox">A Rect bounding box containg all targets.</param>
+    /// <returns>A float for the orthographic size.</returns>
+    float CalculateOrthographicSize(Rect boundingBox)
+    {
+        float orthographicSize = cameraObj.orthographicSize;
+        Vector3 topRight = new Vector3(boundingBox.x + boundingBox.width, boundingBox.y, 0f);
+        Vector3 topRightAsViewport = cameraObj.WorldToViewportPoint(topRight);
+
+        if (topRightAsViewport.x >= topRightAsViewport.y)
+            orthographicSize = Mathf.Abs(boundingBox.width) / cameraObj.aspect / 2f;
+        else
+            orthographicSize = Mathf.Abs(boundingBox.height) / 2f;
+
+        return Mathf.Clamp(Mathf.Lerp(cameraObj.orthographicSize, orthographicSize, Time.deltaTime * zoomSpeed), minimumOrthographicSize, Mathf.Infinity);
+    }
+}
