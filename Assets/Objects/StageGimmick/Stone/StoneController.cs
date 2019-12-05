171a5dbb389c4aa0fb1dbd0852b40ba876923a2f
YdeaGames <ydeagames@gmail.com>
2019-12-06 07:45:03 +0900

    fix

--- Assets/Objects/StageGimmick/Stone/StoneController.cs
+++ Assets/Objects/StageGimmick/Stone/StoneController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoneController : MonoBehaviour
+public class StoneController : Bolt.EntityBehaviour<ITransformState>
 {
     // Start is called before the first frame update
     void Start()
@@ -20,5 +20,10 @@ public class StoneController : MonoBehaviour
                 BoltNetwork.Destroy(gameObject);
         }
 
+    }
+
+    public override void Attached()
+    {
+        state.SetTransforms(state.Transform, transform);
     }
 }
