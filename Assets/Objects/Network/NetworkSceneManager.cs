aeb1cefa66b43693575c03eb2f45801e5cd41c43
MasatoNakashima <nakashimasa16@gmail.com>
2019-12-06 13:00:57 +0900

    stage

--- Assets/Objects/Network/NetworkSceneManager.cs
+++ Assets/Objects/Network/NetworkSceneManager.cs
@@ -49,13 +49,13 @@ namespace RollerBallBolt
                 Destroy(go);
             }
 
-            GameObject[] mos = GameObject.FindGameObjectsWithTag("MovingObject");
-            foreach (GameObject mo in mos)
-            {
-                or.position = mo.transform.position;
-                movingObjectPositions.Add(or);
-                Destroy(mo);
-            }
+            //GameObject[] mos = GameObject.FindGameObjectsWithTag("MovingObject");
+            //foreach (GameObject mo in mos)
+            //{
+            //    or.position = mo.transform.position;
+            //    movingObjectPositions.Add(or);
+            //    Destroy(mo);
+            //}
 
             GameObject ball = GameObject.FindGameObjectWithTag("Ball");
             or.position = ball.transform.position;
