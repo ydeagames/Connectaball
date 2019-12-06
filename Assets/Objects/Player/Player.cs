bc3977e1bc20dd568ed64b348471d4a533a48ff7
soba1 <youshiq@gmail.com>
2019-12-06 11:34:33 +0900

    Sounds

--- Assets/Objects/Player/Player.cs
+++ Assets/Objects/Player/Player.cs
@@ -9,6 +9,8 @@ public class Player : Bolt.EntityBehaviour<ITransformState>
 
     Rigidbody2D rigid;
     public BoxCollider2D box;
+    public AudioClip PlayerBounce;
+    public AudioClip PlayerGrab;
 
     public float speedMultiplier = 0.5f;
     public float angularSpeedMultiplier = 0.5f;
@@ -33,8 +35,19 @@ public class Player : Bolt.EntityBehaviour<ITransformState>
         myAcceleration.x = Input.GetAxis($"JoyX");
         myAcceleration.y = Input.GetAxis($"JoyY");
         myGrabbing = Input.GetButton($"JoyB");
-
+        if(Input.GetButtonDown($"JoyB"))
+        {
+            AudioSource.PlayClipAtPoint(PlayerGrab, transform.position);
+        }
         myAngularAcceleration = Input.GetAxis($"JoyLR");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            AudioSource.PlayClipAtPoint(PlayerBounce, transform.position);
+        }
     }
 
     void FixedUpdate()
